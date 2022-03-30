38e236a54f90463a0aa1507ebd7c0007b1d9b8b7
zfsunk <ixiaoshazi@163.com>
2022-03-30 23:38:09 +0800

    '修改个人信息页面'

--- src/views/profile/PersonalInfoEdit.vue
+++ src/views/profile/PersonalInfoEdit.vue
@@ -3,8 +3,58 @@
     <nav-bar>
       <div slot="left"><van-icon name="arrow-left" /></div>
       <div slot="center">{{ title }}</div>
-      <div slot="right" class="submit">提交</div>
+      <van-button type="default" slot="right" :disabled="isCanSubmit"
+        >提交</van-button
+      >
     </nav-bar>
+    <div v-if="isCurrentEdit == 'name'">
+      <van-field
+        v-model="info.name"
+        clearable
+        placeholder="起个有特点的名字吧"
+        ref="name"
+      />
+    </div>
+    <div v-if="isCurrentEdit == 'intro'">
+      <van-field
+        v-model="info.intro"
+        rows="3"
+        autosize
+        type="textarea"
+        maxlength="70"
+        placeholder="添加简介，让大家更好的认识你"
+        show-word-limit
+        ref="intro"
+      />
+    </div>
+    <div v-if="isCurrentEdit == 'phone'">
+      <van-field label="原手机号" :value="info.phone" readonly />
+      <van-field
+        v-model="newPhone"
+        label="新手机号"
+        type="tel"
+        placeholder="输入手机号"
+        ref="newPhone"
+      >
+        <template #button>
+          <van-button
+            size="normal"
+            type="default"
+            @click="sendSms"
+            :disabled="disabled"
+            >{{ tip }}</van-button
+          >
+        </template></van-field
+      >
+      <van-field
+        v-model="sms"
+        center
+        clearable
+        label="验证码"
+        placeholder="请输入短信验证码"
+      >
+      </van-field>
+    </div>
   </div>
 </template>
 
@@ -15,11 +65,23 @@ export default {
   data() {
     return {
       isCurrentEdit: "name",
+      info: {
+        name: "",
+        intro: "",
+        phone: "11",
+      },
+      newPhone: "",
+      sms: "",
+      tip: "发送验证码",
+      disabled: false,
+      isCanSubmit: false,
     };
   },
   created() {
     console.log(this.$route);
     this.isCurrentEdit = this.$route.query.name;
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    this.info = userInfo;
   },
   computed: {
     title() {
@@ -34,6 +96,26 @@ export default {
           return this.$route.meta.title;
       }
     },
+  },
+  mounted() {
+    this.$nextTick(() => {
+      switch (this.isCurrentEdit) {
+        case "name":
+          this.$refs.name.focus();
+          break;
+        case "phone":
+          this.$refs.newPhone.focus();
+          break;
+        case "intro":
+          this.$refs.intro.focus();
+          break;
+        default:
+          break;
+      }
+    });
+  },
+  methods: {
+    sendSms() {},
   },
   components: {
     NavBar,
